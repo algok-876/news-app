c88c2482e8d540dc37bb77edd77ed126ecae6420
Wang Wei <2219444825@qq.com>
2020-07-19 13:41:24 +0800

    构建优化

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,34 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+      config.set('externals', {
+        vue: 'Vue',
+        axios: 'axios',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex'
+      })
+      config.module.rule('img')
+        .test(/\.(jpg|png|gif)$/)
+        .use('image-webpack-loader')
+        .loader('image-webpack-loader')
+        .options({
+          bypassOnDebug: true
+        })
+        .end()
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    }
   }
 }
